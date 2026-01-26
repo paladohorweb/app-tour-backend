41cf5c031c9b5ec0c18ef9a1bdc79d12918b81ed
paladohorweb <paladohor@gmail.com>
2026-01-26 12:16:19 -0500

    corrigiendo parte 3

--- src/main/java/com/jgm/paladohorweb/tour/service/RefreshTokenService.java
+++ src/main/java/com/jgm/paladohorweb/tour/service/RefreshTokenService.java
@@ -2,11 +2,12 @@ package com.jgm.paladohorweb.tour.service;
 
 import com.jgm.paladohorweb.tour.entity.RefreshToken;
 import com.jgm.paladohorweb.tour.entity.Usuario;
+import com.jgm.paladohorweb.tour.exception.BadRequestException;
+import com.jgm.paladohorweb.tour.exception.ResourceNotFoundException;
 import com.jgm.paladohorweb.tour.repository.RefreshTokenRepository;
 import com.jgm.paladohorweb.tour.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -25,7 +26,8 @@ public class RefreshTokenService {
     public RefreshToken createRefreshToken(Long userId) {
 
         Usuario usuario = usuarioRepository.findById(userId)
-                .orElseThrow(() -> new ChangeSetPersister.NotFoundException("Usuario no encontrado"));
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Usuario no encontrado"));
 
         RefreshToken token = new RefreshToken();
         token.setUsuario(usuario);
@@ -36,23 +38,25 @@ public class RefreshTokenService {
     }
 
     public RefreshToken verifyExpiration(String token) {
+
         RefreshToken refreshToken = repository.findByToken(token)
-                .orElseThrow(() -> new BusinessException("Refresh token inválido"));
+                .orElseThrow(() ->
+                        new BadRequestException("Refresh token inválido"));
 
         if (refreshToken.getExpiryDate().isBefore(Instant.now())) {
             repository.delete(refreshToken);
-            throw new BusinessException("Refresh token expirado");
+            throw new BadRequestException("Refresh token expirado");
         }
 
         return refreshToken;
     }
 
     public void deleteByUser(Long userId) {
+
         Usuario usuario = usuarioRepository.findById(userId)
-                .orElseThrow(() -> new ChangeSetPersister.NotFoundException("Usuario no encontrado"));
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Usuario no encontrado"));
 
         repository.deleteByUsuario(usuario);
     }
 }
-
-
