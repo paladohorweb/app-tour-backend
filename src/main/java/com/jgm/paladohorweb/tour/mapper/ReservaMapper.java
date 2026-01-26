f244b646aae44d97c25cc2ef43ae36e6a5f913c8
paladohorweb <paladohor@gmail.com>
2026-01-26 10:04:15 -0500

    agregando Mapper Reserva y DTO Reserva

---
+++ src/main/java/com/jgm/paladohorweb/tour/mapper/ReservaMapper.java
@@ -0,0 +1,11 @@
+package com.jgm.paladohorweb.tour.mapper;
+
+import com.jgm.paladohorweb.tour.entity.Reserva;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ReservaMapper {
+    @Mapping(source = "tour.titulo", target = "tour")
+    ReservaResponse toResponse(Reserva entity);
+}
